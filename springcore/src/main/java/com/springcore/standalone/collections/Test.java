911142cbd0ad6d6b6b63bbb6a350f291ecc9169c
aa185465 <aa185465@WINAA185465-N7H.corp.ncr.com>
2020-08-28 16:15:55 +0530

    Implemented standalone collections using util Schema.

--- springcore/src/main/java/com/springcore/standalone/collections/Test.java
+++ springcore/src/main/java/com/springcore/standalone/collections/Test.java
@@ -1,9 +1,17 @@
 package com.springcore.standalone.collections;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/standalone/collections/config.xml");
+		
+		Person p1=context.getBean("person1",Person.class);
+		System.out.println(p1);
+		System.out.println(p1.getFriends().getClass().getName());
 
 	}
 
